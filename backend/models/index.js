10e60954b6ca348cf39a9e425adaa21346562edb
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2025-12-29 15:55:09 +0900

    폴더생성, 로컬 회원가입 구현

---
+++ backend/models/index.js
@@ -0,0 +1,11 @@
+const User = require("./User.model");
+const Deal = require("./Deal.model");
+const Team = require("./Team.model");
+const TeamMember = require("./TeamMember.model");
+
+module.exports = {
+    User,
+    Deal,
+    Team,
+    TeamMember,
+};
