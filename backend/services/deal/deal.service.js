870007fa0c07ee86c576291bceccb3b3ddc60c3b
tmakdrl <tmakdrl@naver.com>
2026-01-05 17:59:18 +0900

    features: deal finish

--- backend/services/deal/deal.service.js
+++ backend/services/deal/deal.service.js
@@ -1,9 +1,72 @@
+const Deal = require("../../models/Deal.model");
+
 // 영수증 등록 POST /deal/register
+const createDeal = async (dealData) => {
+  try {
+    const deal = new Deal(dealData);
+    await deal.save();
+    return deal;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // 영수증 조회 GET /deal/:id
+const getDealDetail = async (dealId) => {
+  try {
+    const deal = await Deal.findById(dealId);
+    return deal;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// // 영수증 월별조회 GET /deal/monthly?year=&month=
+const getMonthlyDeals = async (userId, year, month) => {
+  try {
+    const startDate = new Date(year, month - 1, 1);
+    const endDate = new Date(year, month, 1);
+
+    const deals = await Deal.find({
+      u_id: userId, // 이거 일단 userId로 했는데 teamId로 바꿔야 됨(영수증쪽에서 userId가 굳이 필요한가...?)
+      date: {
+        $gte: startDate,
+        $lt: endDate,
+      },
+    }).sort({ date: -1 });
+
+    return deals;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // 영수증 수정 PUT /deal/:id
+const updateDeal = async (dealId, updateData) => {
+  try {
+    const updatedDeal = await Deal.findByIdAndUpdate(dealId, updateData, {
+      new: true,
+    });
+    return updatedDeal;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // 영수증 삭제 DELETE /deal/:id
+const deleteDeal = async (dealId) => {
+  try {
+    const deletedDeal = await Deal.findByIdAndDelete(dealId);
+    return deletedDeal;
+  } catch (error) {
+    throw error;
+  }
+};
 
-// 영수증 월별조회 GET /deal/monthly?year=&month=
+module.exports = {
+  createDeal,
+  getDealDetail,
+  getMonthlyDeals,
+  updateDeal,
+  deleteDeal,
+};
