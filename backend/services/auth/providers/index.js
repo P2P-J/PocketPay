ea5a6679208a9eeacd70194d91d67177455b62bc
tmakdrl <tmakdrl@naver.com>
2026-01-05 18:11:18 +0900

    Revert "Merge pull request #9 from P2P-J/features/deal"
    This reverts commit 24bbb6e80cfd2a8f9134d67add25697d0845c2be, reversing
    changes made to 76e44ee901f7ea3c296a81a2e031bc8b800820e8.

--- backend/services/auth/providers/index.js
+++
@@ -1,9 +0,0 @@
-const google = require("./google.provider");
-const kakao = require("./kakao.provider");
-const naver = require("./naver.provider");
-
-module.exports = {
-    google,
-    kakao,
-    naver,
-};
