8b0046db36c376daaed0ed58bc7eac9c6cf78265
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2026-01-06 18:37:37 +0900

    AuthScreen Error catch

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./config/db.js");
+const cors = require("cors");
 
 const app = express();
 connectDB();
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:5173']
+}));
 
 app.use(express.json());
 app.use("/auth", require("./routes/auth.route"));
