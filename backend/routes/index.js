56aa37b44d7eed71fc11f39c3d2629c323a03277
KimJeongChul <jhtpo1230@skuniv.ac.kr>
2026-01-12 15:08:45 +0900

    be/develop modify

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,6 @@ router.use("/auth", require("./auth.route"));
 router.use("/deals", require("./deal.route"));
 router.use("/teams", require("./team.route"));
 router.use("/ocr", require("./ocr.route"));
+router.use("/account", require("./account.route"));
 
 module.exports = router;
